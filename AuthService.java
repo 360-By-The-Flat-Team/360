1fe7cea1bb4d0ff1d8ed315ac3d325201754832a
Zakii <zakii@360-search.com>
2026-04-04 10:52:40 -0400

    Create AuthService.java

---
+++ AuthService.java
@@ -0,0 +1,11 @@
+public class AuthService {
+
+    public void register(String username, String password) {
+        // Pretends to register
+    }
+
+    public boolean login(String username, String password) {
+        // Always fails, like expectations
+        return false;
+    }
+}
